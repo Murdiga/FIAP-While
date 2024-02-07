f5c5c471e265852a95d2acbc471a4399797fa70c
unknown <guimurdiga@hotmail.com>
2024-02-07 11:05:48 -0300

    primeiro exercicio

---
+++ TesteWhile.java
@@ -0,0 +1,16 @@
+package br.com.fiap.whl;
+
+public class TesteWhile {
+	
+	public static void main(String[] args) {
+		
+		int num = 0;
+		
+		while (num >= 0 && num < 100){
+			num++;
+			System.out.println(num);
+		}
+		
+	}
+	
+}
